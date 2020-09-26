d34d9da567151007984d347ad882de9bc28ce4ed
Peter Lillevold <peter.lillevold@4subsea.com>
2020-09-26 13:40:09 +0200

    Examples on upload and download

--- Connector/App.cs
+++ Connector/App.cs
@@ -21,6 +21,20 @@ namespace Sample
 
         public async Task Run()
         {
+            var timeSeriesId = "<timeseries id>";
+
+            await AddDataToTimeSeries(timeSeriesId);
+            await DownloadTimeSeriesData(timeSeriesId);
+        }
+
+        /// <summary>
+        /// Shows how to add data to an existing series.
+        /// Data is structured as CSV where each line represent one sample.
+        /// Each sample have a timestamp and a value, where timestamp is in nanoseconds since EPOCH.
+        /// Value can be number or text data.
+        /// </summary>
+        private async Task AddDataToTimeSeries(string timeSeriesId)
+        {
             Guid fileId;
             await using (var timeSeriesContent = ContentGenerator.GenerateRandomSeries(DateTimeOffset.UtcNow, 1, 12))
             {
@@ -31,12 +45,22 @@ namespace Sample
 
             await _file.WaitForFileProcessed(fileId);
 
-            var timeSeriesId = "<timeseries id or alias>";
-
             Console.WriteLine($"Appending {fileId} to {timeSeriesId}...");
-            await _timeSeries.Append(fileId, timeSeriesId);
+            await _timeSeries.AddData(fileId, timeSeriesId);
+        }
 
-            Console.ReadLine();
+        /// <summary>
+        /// Shows how to download data from a series.
+        /// </summary>
+        private async Task DownloadTimeSeriesData(string timeSeriesId)
+        {
+            var start = new DateTimeOffset(2010, 9, 22, 0, 0, 0, TimeSpan.Zero);
+            var end = new DateTimeOffset(2010, 9, 26, 0, 0, 0, TimeSpan.Zero);
+            var data = _timeSeries.GetData(timeSeriesId, start, end);
+            await foreach (var chunk in data)
+            {
+                Console.WriteLine(chunk);
+            }
         }
     }
 }
