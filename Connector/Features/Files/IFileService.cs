1a9f97c65f4b14b8c1bc1718389497f7a0ee5cca
Peter Lillevold <peter.lillevold@4subsea.com>
2020-09-26 14:09:07 +0200

    Doc on FileService

--- Connector/Features/Files/IFileService.cs
+++ Connector/Features/Files/IFileService.cs
@@ -6,7 +6,14 @@ namespace Sample.Features.Files
 {
     public interface IFileService
     {
+        /// <summary>
+        /// Upload data to a file and return the file id for later adding to a time series.
+        /// </summary>
         Task<Guid> Upload(Stream timeSeriesContent);
+
+        /// <summary>
+        /// Check processing state of a file and return when the state is either Ready or Failed.
+        /// </summary>
         Task WaitForFileProcessed(Guid fileId);
     }
 }
